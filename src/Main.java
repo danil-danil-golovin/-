8c9d4892629281ad79cc70b5731cb650ccd8e2c9
danil <danil.danil.golovin@mail.ru>
2023-11-26 21:37:12 +0300

    Строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName);
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+        String TrueFullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — "  + TrueFullName);
     }
 }
